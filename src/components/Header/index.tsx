4c9a03dd2c6852d74fd11eec24c80885fa28a1b5
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-04-28 19:20:17 -0300

    header estilizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content } from './styles';
+
 export function Header() {
   return (
-    <header>
-      Header
-    </header>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button>
+          Nova transação
+      </button>
+      </Content>
+    </Container>
   );
 }
