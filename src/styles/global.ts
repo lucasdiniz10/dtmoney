86df4b262bd9181dea0c1c359382d0637fcd3b04
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-05-04 17:43:22 -0300

    estilizacao inicial conteudo modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -70,6 +70,11 @@ export const GlobalStyle = createGlobalStyle`
   }
   
   .react-modal-content {
-
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
   }
 `
