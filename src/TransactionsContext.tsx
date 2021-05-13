b1f94eecf3745a2619f6d38cc751b5d82bb92660
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-05-13 00:34:58 -0300

    carregando dados com contexto

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { api } from './services/api';
 
 interface Transaction {
@@ -10,13 +10,23 @@ interface Transaction {
   createdAt: string;
 }
 
-export const TransactionsContext = createContext([]);
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
 
-export function TransactionsProvider() {
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
     api.get('transactions')
       .then(response => setTransactions(response.data.transactions))
   }, []);
+
+  return (
+    <TransactionsContext.Provider value={transactions}>
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
